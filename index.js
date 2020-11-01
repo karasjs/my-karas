6a42b44329b1979345bed62765b9d15d3ffe2fb4
army8735 <army8735@qq.com>
2020-11-01 16:56:07 +0800

    feat: 0.40

--- index.js
+++ index.js
@@ -135,7 +135,7 @@
     return _get(target, property, receiver || target);
   }
 
-  var version = "0.38.3";
+  var version = "0.40.0";
 
   karas.inject.requestAnimationFrame = function (cb) {
     setTimeout(cb, 1000 / 60);
